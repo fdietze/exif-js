da3116ce3e3a32955e0319dfdd3f7a27e25cfe5f
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-09-11 23:55:16 +0200

    move n out of problematic for loop

--- exif.js
+++ exif.js
@@ -736,7 +736,8 @@
 
     function getStringFromDB(buffer, start, length) {
         var outstr = "";
-        for (var n = start; n < start+length; n++) {
+        var n = start;
+        for (; n < start+length; n++) {
             outstr += String.fromCharCode(buffer.getUint8(n));
         }
         return outstr;
